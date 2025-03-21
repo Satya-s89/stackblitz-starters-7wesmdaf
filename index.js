52fdd8ddef35f4585084563ef8888117e30edde9
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-21 11:46:06 +0530

    7

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+async function connectDatabase(){
+  try {
+    await mongoose.connect('process.env.MONGOOSE_URL')
+    res.status(200).send("Database connected")
+  } catch (error) {
+    res.status(400).send({msg:"Error"},error)
+  }
+}
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
